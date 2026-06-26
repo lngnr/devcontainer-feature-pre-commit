2c380f93cb499e4fd3cdd8199371f9a0f97641a7
Jonas Eddy Langner <jonas.langner23@gmail.com>
2026-06-26 13:36:46 +0000

    refac: moved installation dir.

--- src/pre-commit/install.sh
+++ src/pre-commit/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-mkdir -p ~/.python3/venv
-python3 -m venv ~/.python3/venv
-~/.python3/venv/bin/pip install pre-commit
-ln -s ~/.python3/venv/bin/pre-commit /usr/local/bin/pre-commit
+mkdir -p /opt/pre-commit/venv
+python3 -m venv /opt/pre-commit/venv
+/opt/pre-commit/venv/bin/pip install pre-commit
+ln -s /opt/pre-commit/venv/bin/pre-commit /usr/local/bin/pre-commit
 pre-commit --version
